132de64133bbefc308f7317a56c7c3a15ee4f9ef
AdamGit1 <brown.adam360@yahoo.com>
2020-06-16 17:52:44 -0500

    message

--- ExcerseThree/Program.cs
+++ ExcerseThree/Program.cs
@@ -6,7 +6,28 @@ namespace ExcerseThree
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a number!: ");
+
+            String userInput = Console.ReadLine();
+
+            int number = int.Parse(userInput);
+            Console.WriteLine(number);
+
+            while (number != 1)
+            {
+                if (number % 2 == 0)
+            {
+                number = number / 2;
+                Console.WriteLine(number);
+            }
+            else
+            {
+                number = number * 3 + 1;
+                Console.WriteLine(number);
+            }
+        
+
+            }
         }
     }
 }
